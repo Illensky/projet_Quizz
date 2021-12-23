3478d1f14360e15400640126934fa19fca6668e3
Illensky <alexis.laroche.02240@gmail.com>
2021-12-23 11:07:11 +0100

    Add questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,13 @@ const questionary = [
     ["Quelle est la capitale de la France ?", ["Marseille", "Nantes", "Paris", "Lille"], "Paris"],
     ["Quelle est la capitale de l'Italie ?", ["Naples", "Rome", "Milan", "Florence"], "Rome"],
     ["Quelle est la capitale de l'Espagne ?", ["Madrid", "Seville", "Barcelone", "Valence"], "Madrid"],
-    ["Quelle est la capitale de la Belgique ?", ["Namur", "Antwerp", "Ghent", "Brussels"], "Brussels"]
+    ["Quelle est la capitale de la Belgique ?", ["Namur", "Antwerp", "Ghent", "Brussels"], "Brussels"],
+    ["Quelle est la capitale de la Pologne ?", ["Wroclaw", "Varsovie", "Lublin", "Krakovie"], "Varsovie"],
+    ["Quelle est la capitale de la Chine ?", ["Pékin", "Xi'An", "Zhengzhou", "Jinan"], "Pékin"],
+    ["Quelle est la capitale de la Hollande ?", ["La Hague", "Tilburg", "Amsterdam", "Rotterdam"], "Amsterdam"],
+    ["Quelle est la capitale des Etats Unis ?", ["New-York", "Houston", "Los Angeles", "Washington"], "Washington"],
+    ["Quelle est la capitale du Canada ?", ["Montreal", "Ottawa", "Quebec City", "Toronto"], "Ottawa"],
+    ["Quelle est la capitale du Maroc ?", ["Rabat", "Casablanca", "Fes", "Marakech"], "Rabat"]
 ];
 
 let score = 0;
@@ -25,6 +31,7 @@ function runQuestionary (el, questionary, startBtn) {
         questionaryDiv.appendChild(questionP)
 
         const answersDiv = document.createElement("div");
+        answersDiv.id = "answerDiv"
         for (let answer of questionary[questionI][1]) {
             const answerP = document.createElement("p");
             answerP.innerHTML = answer;
@@ -56,19 +63,25 @@ function runQuestionary (el, questionary, startBtn) {
 
         const scoreDiv = document.createElement("div");
         scoreDiv.innerHTML = "score : " + score.toString() + " / " + questionary.length.toString();
+        score = 0;
+        scoreDiv.id = "scoreDiv"
         questionaryDiv.appendChild(scoreDiv);
 
         const errorDiv = document.createElement("div");
+        errorDiv.id = "errorDiv"
         for (let error of wrongAnswer) {
             const errorP = document.createElement('p');
             errorP.innerHTML = "Erreur a la question : <br>" + questionary[error[0]][0] + "<br> Vous avez répondu : " + error[1] +
             "<br> La bonne réponse était : " + questionary[error[0]][2];
             errorDiv.appendChild(errorP);
         }
+        wrongAnswer = [];
         questionaryDiv.appendChild(errorDiv);
 
         startBtn.innerHTML = "Re-jouer";
         startBtn.style.display = "inline";
+
+
     }
 }
 
